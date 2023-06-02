8c25fe70ac877ec6f794b31c3d490619ba93ac28
Kyrylo Herasymenko <heraskyr@fel.cvut.cz>
2023-06-02 22:23:20 +0200

    Commented code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+/**
+ * Note class
+ */
 class Note {
     constructor(text, header, time, urgency, id) {
         this.text = text;
@@ -8,29 +12,47 @@ class Note {
         this.deleted = false;
     }
 
+    /**
+     * Method to turn note into undraggable html object
+     * @returns string html version of note object
+     */
     toHtml() {
         if (this.deleted) return "";
         return `<div id = '${this.id}' class = 'note ${this.urgency ? 'urgent' : ''}'><h1>${this.header}</h1><h3 class = delete onclick = deleteNote(event)>delete</h2><p>${this.text}</p><p>Target time is ${this.time}</p></div>`;
     }
 
+    /**
+     * Method to turn note into draggable html object
+     * @returns string html version of note object
+     */
     toHtmlDraggable() {
         if (this.deleted) return "";
         return `<div ondragstart="drag(event)" id = '${this.id}' draggable = 'true' class = 'note ${this.urgency ? 'urgent' : ''}'><h1>${this.header}</h1><p>${this.text}</p><p>Created on ${this.time}</p></div>`;
     }
 }
 
+
+/**
+ * Button to turn on menu on small screens
+ */
 class MenuSVG {
     constructor() {
         this.firstline = document.getElementById("toggle-menu-svg").childNodes[1];
         this.secondline = document.getElementById("toggle-menu-svg").childNodes[3];
     }
 
+    /**
+     * Menu opened and button makes arrow
+     */
     makeArrow() {
         this.firstline.setAttribute("transform", "rotate(-16, 40, 12)");
         this.secondline.setAttribute("transform", "rotate(16, 40, 28)");
         console.log("Making arrow")
     }
 
+    /**
+     * Menu closed and button returns to original position
+     */
     makeLines() {
         this.firstline.setAttribute("transform", "rotate(0, 40, 12)");
         this.secondline.setAttribute("transform", "rotate(0, 40, 28)");
@@ -38,6 +60,10 @@ class MenuSVG {
 
 }
 
+
+/**
+ * Form for getting new notes
+ */
 class NoteForm {
 
 
@@ -48,6 +74,11 @@ class NoteForm {
         this.urgency = document.getElementById('urgency');
     }
 
+
+    /**
+     * Gets data from form and makes it a note
+     * @returns note 
+     */
     getData() {
         let date = this.time.value;
         
@@ -70,6 +101,10 @@ class NoteForm {
     }
 }
 
+
+/**
+ * Class for page routing
+ */
 class PageRouter {
     constructor(pages, defaultPage) {
         this.pages = pages;
@@ -80,22 +115,43 @@ class PageRouter {
         this.route(window.location.href);
 
 
+        /**
+         * History is changing => user wants to change page
+         * route by url
+         */
         window.addEventListener('popstate', e => { 
             this.route(window.location.href);
-}
+        }
         )
 
-        window.addEventListener('click', e => {
-            const element = e.target;
-            if (element.nodeName == 'A') {
-                e.preventDefault();
-                this.route(element.href);
-                window.history.pushState(null, null, element.href);
-                console.log("routed by a");
-            }
 
+        /**
+         * Add all in-page links different behaviour
+         */
+
+        document.querySelectorAll('header a').forEach(link => {
+            link.addEventListener('click', e => {
+                e.preventDefault();
+                this.route(link.href);
+                window.history.pushState(null, null, link.href);
+                //console.log("routed by a");
+            })
         })
+
+         document.querySelectorAll('aside a').forEach(link => {
+            link.addEventListener('click', e => {
+                e.preventDefault();
+                this.route(link.href);
+                window.history.pushState(null, null, link.href);
+            })
+        })
+        
     }
+
+    /**
+     * Changes page
+     * @param {url to locate} urlString 
+     */
     route(urlString) {
         const url = new URL(urlString)
         const page = url.searchParams.get('page')
@@ -104,11 +160,16 @@ class PageRouter {
             this.actualPage.hidePage();
         }
 
-        this.actualPage = this.pages.find(p => p.key === (page ?? this.defaultPage)) ?? this.errorPage;
+        this.actualPage = this.pages.find(p => p.key === (page ?? this.defaultPage));
+        if (!this.actualPage) this.actualPage =  this.errorPage;
         this.actualPage.showPage();
     }
 }
 
+
+/**
+ * Main class for pages with showPage and hidePage methods
+ */
 class Page {
     key
     title
@@ -119,23 +180,36 @@ class Page {
         this.pageElement = document.getElementsByClassName('maincontainer')[0];
     }
 
+
+    /**
+     * Draw this page in main container
+     */
     showPage() {
         document.title = this.title;
         this.pageElement.innerHTML = this.gethtml();
 
     }
 
+    /**
+     * Remove this page from main container 
+     */
     hidePage() {
         this.pageElement.innerHTML = '';
     }
 
+    /**
+     * 
+     * @returns HTML of this page
+     */
     gethtml() {
         return "";
     }
 }
 
 
-
+/**
+ * All notes page that inherits Page class
+ */
 class AllNotesPage extends Page {
     constructor(settings) {
         super(settings);
@@ -149,6 +223,9 @@ class AllNotesPage extends Page {
     }
 }
 
+/**
+ * Change urgency page inherits Page class
+ */
 class ChangeUrgencyPage extends Page {
     constructor(settings) {
         super(settings);
@@ -168,6 +245,10 @@ class ChangeUrgencyPage extends Page {
     }
 }
 
+
+/**
+ * Error page inherits Page class
+ */
 class ErrorPage extends Page {
     constructor(settings) {
         super(settings);
@@ -177,6 +258,10 @@ class ErrorPage extends Page {
     }
 }
 
+
+/**
+ * Form page inherits Page class
+ */
 class FormPage extends Page {
     constructor(settings) {
         super(settings);
@@ -223,6 +308,10 @@ class FormPage extends Page {
     }
 }
 
+
+/**
+ * If user is offline, he must know
+ */
 window.addEventListener("offline", e => addOffline())
 function addOffline()
 {
@@ -232,6 +321,9 @@ function addOffline()
 /**
  * Beginning variables
  * */
+
+
+// Load notes from local storage
 var notes = (function loadNotes() {
     const fromStorage = JSON.parse(localStorage.getItem("notes"));
     if (fromStorage == undefined) return [];
@@ -245,19 +337,23 @@ var notes = (function loadNotes() {
 })()
 
 let menuToggled = false;
+// Make menu button
 const menusvg = new MenuSVG();
 const sidemenu = document.getElementsByClassName('sidemenu')[0];
 const showContainer = document.getElementById("maincontainer");
 const toggleMenuDiv = document.getElementById("toggle-menu-svg");
+// Make form for adding new notes
 let noteForm = new NoteForm();
 
 /**
- * 
  * Functions
  * */
 
-function changeSVG() {
 
+/**
+ * Change menu button
+ */
+function changeSVG() {
     if (menuToggled) {
         menusvg.makeArrow();
     }
@@ -266,6 +362,10 @@ function changeSVG() {
     }
 }
 
+
+/**
+ * toggle left menu 
+ */
 function toggleMenu() {
     if (menuToggled) {
         sidemenu.removeAttribute("style")
@@ -273,7 +373,6 @@ function toggleMenu() {
     }
     else {
         sidemenu.setAttribute("style", "left:-200px")
-
         sidemenu.classList.add('hidden-side-menu')
     }
 }
@@ -285,6 +384,40 @@ function drag(ev) {
 function allowDrop(e) {
     e.preventDefault();
 }
+
+function drop(e) {
+    e.preventDefault();
+    const elementId = e.dataTransfer.getData("text");
+    const element = document.getElementById(elementId);
+    console.log(e.target.classList)
+    if (e.target.classList.contains("urgent")) {
+        notes.map(elem => {
+            if (elem.id == elementId) {
+                elem.urgency = true;
+            }
+            return e;
+        })
+        element.classList.add('urgent');
+    }
+    else {
+        notes.map(elem => {
+            if (elem.id == elementId) {
+                elem.urgency = false;
+            }
+            return elem;
+        })
+        element.classList.remove('urgent');
+
+    }
+    updateStorage();
+    e.target.appendChild(element);
+}
+
+/**
+ * Prevents XSS
+ * @param {oldText} text 
+ * @returns text without XSS vulnerabilities
+ */
 function escapehtml(text) {
     return text
         .replace(/</g, "&lt;")
@@ -292,6 +425,10 @@ function escapehtml(text) {
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&#039;");
   }
+
+  /**
+   * Sends HTTP request to server to get notes
+   */
 function loadNotesFromRemote()
 {
     alert("Notes are updating. Please wait for the next alert");
@@ -326,6 +463,9 @@ function loadNotesFromRemote()
     
 }
 
+/**
+   * Sends HTTP request to server to send notes
+   */
 function uploadNotesToRemote()
 {
     alert("Notes are updating. Please wait for the next alert");
@@ -348,38 +488,16 @@ function uploadNotesToRemote()
     .catch(error => alert(error));
 }
 
+
+/**
+ * Update notes in local storage
+ */
 function updateStorage() {
     let jsonList = JSON.stringify(notes);
     localStorage.setItem('notes', jsonList);
 }
 
-function drop(e) {
-    e.preventDefault();
-    const elementId = e.dataTransfer.getData("text");
-    const element = document.getElementById(elementId);
-    console.log(e.target.classList)
-    if (e.target.classList.contains("urgent")) {
-        notes.map(elem => {
-            if (elem.id == elementId) {
-                elem.urgency = true;
-            }
-            return e;
-        })
-        element.classList.add('urgent');
-    }
-    else {
-        notes.map(elem => {
-            if (elem.id == elementId) {
-                elem.urgency = false;
-            }
-            return elem;
-        })
-        element.classList.remove('urgent');
 
-    }
-    updateStorage();
-    e.target.appendChild(element);
-}
 
 function deleteNote(e)
 {
@@ -408,13 +526,7 @@ document.getElementById('download').addEventListener('click', e => loadNotesFrom
 document.getElementById('upload').addEventListener('click', e => uploadNotesToRemote());
 
 
-function drawNotes()
-{
-    document.getElementsByClassName('notecontainer').innerHTML = '';
-    notes.forEach(element => document.getElementsByClassName('notecontainer').innerHTML += element.toHtml());
-}
 
-drawNotes();
 const pageRouter = new PageRouter(
     [
         new FormPage({ key: 'add', title: 'Add note!' }),
